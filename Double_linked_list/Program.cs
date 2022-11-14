8ef4022bec55a86ae54d1beba5163e6d275a9f5e
Aditya Hari <aditya.hari.ft21@mail.umy.ac.id>
2022-11-14 17:36:19 +0700

    add program on 'addNode' Method

--- Double_linked_list/Program.cs
+++ Double_linked_list/Program.cs
@@ -30,7 +30,53 @@ namespace Double_linked_list
         }
         public void addNode()
         {
-            
+            int nim;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnterthe name of the student: ");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.noMhs = nim;
+            newNode.name = nm;
+
+            if (START == null || nim == START.noMhs)
+            {
+                if ((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nDuplicate number not allowed");
+                    return;
+                }
+                newNode.next = START;
+                if (START != null)
+                    START.prev = newNode;
+                newNode.prev = null;
+                START = newNode;
+                return;
+            }
+            //if the node is to be inverted at between two node
+            Node previous, current;
+            for (current = previous = START;
+                current != null && nim >= current.noMhs;
+                previous = current, current = current.next)
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
+            newNode.next = current;
+            newNode.prev = previous;
+
+            if (current == null)
+            {
+                newNode.next = current;
+                previous.next = newNode;
+                return;
+            }
+            current.prev = newNode;
+            previous.next = newNode;
         }
         
     }
